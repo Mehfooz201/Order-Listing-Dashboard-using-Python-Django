19b62d490a7656708106268eec1834ea80ede118
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-08-11 01:35:07 +0500

    changes

--- amruloapp/admin.py
+++ amruloapp/admin.py
@@ -5,7 +5,7 @@
 
 
 from django.contrib import admin
-from .models import User, Order
+from .models import User, Order, StaffUser
 
 #Registered here
 
@@ -18,3 +18,7 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = ('order_number', 'customer_name', 'order_status', 'salesman', 'order_date')
 
 admin.site.register(Order, OrderAdmin)
+
+class StaffUserAdmin(admin.ModelAdmin):
+    list_display = ('approval_status',)
+admin.site.register(StaffUser, StaffUserAdmin)
