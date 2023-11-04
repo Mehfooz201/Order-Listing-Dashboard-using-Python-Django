6945b65b9fa67adaea9d9ce2445bf80847d5a1e5
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-11-05 02:24:33 +0500

    upd

--- Amrulo/settings.py
+++ Amrulo/settings.py
@@ -84,13 +84,23 @@ WSGI_APPLICATION = "Amrulo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cdl_database',
+        'USER': 'postgres',
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'localhost',  # Change this if your PostgreSQL server is on a different host
+        'PORT': '5432',  # Set the port if not using the default 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
