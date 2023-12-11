96405866445c4d08a13ce51956a1d3ca51f7a4d2
Novendobi <nchidinma4@gmail.com>
2023-12-11 13:50:11 +0100

    forprod

--- Amrulo/settings.py
+++ Amrulo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY_2')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','10.0.0.6']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
